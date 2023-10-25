12007a25db532384910b6dec8f1459520fa3a038
Adik <adikabi907@gmail.com>
2023-10-25 17:06:27 +0700

    pertemuan 1

---
+++ pertemuan 1/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int garisAtas = 45;
+  for (int a = 0; a < garisAtas; a++) {
+    cout << "-";
+  } cout << endl;
+
+    cout << "Nama : Adik Abimaulana\n\n" << endl;
+    cout << "NPM : 2310631170063\n\n" << endl;
+    cout << "Kelas : C - Informatika\n\n" << endl;
+    cout << "Cita - cita : Programmer\n\n" << endl;
+    cout << "Motivasi Masuk Informatika : Ingin belajar IT" << endl;
+
+     int garisBawah = 45;
+  for (int a = 0; a < garisBawah; a++) {
+    cout << "-";
+  } cout << endl;
+    return 0;
+}
